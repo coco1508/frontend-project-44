0aebeb0255d23ce3a12b03e100f3fb7c05028818
kostya <kostya.petryaev114@gmail.com>
2023-03-31 14:13:29 +0300

    .

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,3 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!');
+import hi from '../src/cli.js'
+console.log(hi());
---
+++ src/cli.js
@@ -0,0 +1,7 @@
+import readlineSync from 'readline-sync';
+ const hi = () => {
+    console.log('Welcome to the brain-games')
+    const name = readlineSync.question('What`s your name? ');
+    return (`hello ${name}`)
+};
+ export default hi;
