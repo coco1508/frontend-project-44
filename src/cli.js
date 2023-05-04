83f4f5a75f66b9992900f67b93a4c7a34d3f4d54
kostya <kostya.petryaev114@gmail.com>
2023-05-04 13:53:49 +0300

    lagged

--- src/cli.js
+++ src/cli.js
@@ -3,6 +3,6 @@ import readlineSync from 'readline-sync';
 const hi = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
-  return (`${name}`);
+  console.log(`Hello ${name}`);
 };
 export default hi;
