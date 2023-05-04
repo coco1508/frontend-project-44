c48b76349dd92bd37c501f9bd571fc24eabbf1d5
kostya <kostya.petryaev114@gmail.com>
2023-05-04 12:24:06 +0300

    finish1

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const hi = () => {
-  console.log('Welcome to the brain-games!');
+  console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('What`s your name? ');
   return (`${name}`);
 };
