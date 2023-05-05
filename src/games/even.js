6b097628a36b457d17237bfa4017a01bceeb6a78
kostya <kostya.petryaev114@gmail.com>
2023-05-05 10:45:13 +0300

    lagged2

--- src/games/even.js
+++ src/games/even.js
@@ -5,16 +5,16 @@ import getRandomIntInclusive from '../util.js';
 const isEven = (number) => (number % 2 === 0);
 const evenGame = () => {
   const name = hi();
-  console.log('Answer "yes" if the number is even, another "no".');
+  console.log('Answer "yes" if the number is even, otherwise "no".');
   for (let i = 0; i < 3; i += 1) {
     const randomNumber = getRandomIntInclusive(1, 100);
     console.log(`Question: ${randomNumber}`);
     const answer = readlineSync.question('Your answer: ');
     const expectedAnswer = isEven(randomNumber) ? 'yes' : 'no';
     if (answer === expectedAnswer) {
-      console.log('Good job!');
+      console.log('Correct!');
     } else {
-      console.log(`'${answer}' is wrong answer. Correct answer was '${expectedAnswer}'.`);
+      console.log(`'${answer}' 'yes' is wrong answer ;(. Correct answer was 'no'. '${expectedAnswer}'.`);
       return console.log(`Let's try again, ${name}!`);
     }
   }
